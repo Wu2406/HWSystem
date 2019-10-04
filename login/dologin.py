2708e69c458e4297c7a86aa7f4c0a45c1fc42bdb
Wu2406 <wu2406@outlook.com>
2019-10-04 20:19:18 +0800

    Add a login module

---
+++ login/dologin.py
@@ -0,0 +1,5 @@
+def do_the_login(username,password):
+     if username=='admin' and password=='admin':
+         return "welcome!"
+     else: 
+         return "error!"
--- testflask.py
+++ testflask.py
@@ -1,10 +1,25 @@
-from flask import Flask
- 
-app = Flask(__name__)
+from flask import Flask,request
+import json
+from login.dologin import do_the_login
+app = Flask(__name__,static_url_path='')
  
 @app.route('/')
 def hello_world():
     return 'Hello World!'
- 
+
+@app.route('/login')
+def logins():
+    return app.send_static_file('login.html')
+
+@app.route('/dologin',methods=['POST'])
+def dologin():
+    data=request.get_data()
+    print("data = %s" %data)
+    username = request.form['user']
+    password = request.form['pwd']
+    print("username:"+username)
+    print("password"+password)
+    return do_the_login(username,password)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
