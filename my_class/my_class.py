c3b53e6b5ce5a4b612999a5437c7aec421eb39c8
Wu2406 <wu2406@outlook.com>
2019-10-04 21:09:50 +0800

    Add a print class module

---
+++ my_class/my_class.py
@@ -0,0 +1,10 @@
+def printclass(day,username):
+    classlist = [{"id": "第一节课", "value": "语文"},
+               {"id": "第二节课", "value": "数学"},
+               {"id": "第三节课", "value": "道法"},
+               {"id": "第四节课", "value": "物理"},
+               {"id": "第五节课", "value": "英语"},
+               {"id": "第六节课", "value": "历史"},
+               {"id": "第七节课", "value": "地理"},
+               {"id": "第八节课", "value": "体育"},]
+    return classlist
--- testflask.py
+++ testflask.py
@@ -1,11 +1,21 @@
 from flask import Flask,request
 import json
 from login.dologin import do_the_login
+from flask import render_template
+from my_class.my_class import printclass
+
+
 app = Flask(__name__,static_url_path='')
  
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/myclass')
+def my_class():
+    d='monday'
+    username='admin'
+    return render_template('my_class.html',day=d,my_list=printclass(d,username))
 
 @app.route('/login')
 def logins():
